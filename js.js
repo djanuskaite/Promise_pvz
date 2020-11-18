26ae450242e356bbe61d8ead41becd817d09648a
djanuskaite <januskaite.domante@gmail.com>
2020-11-18 10:37:38 +0200

    Added example

--- js.js
+++ js.js
@@ -1,20 +1,31 @@
-let pazadas = new Promise(function (resolve, reject){
-    kompoKaina = 20000; // galim keisti
-    santaupos = 30000; // galim keisti
-    if (santaupos > kompoKaina) {
-        resolve({
-            brand: "MacBook",
-            model: "Pro"
-        });
+// let pazadas = new Promise(function (resolve, reject){
+//     kompoKaina = 20000; // galim keisti
+//     santaupos = 30000; // galim keisti
+//     if (santaupos > kompoKaina) {
+//         resolve({
+//             brand: "MacBook",
+//             model: "Pro"
+//         });
+//     } else {
+//         reject("Neturi pakankamai pinigu");
+//     }
+// }); pazadas.then(function (value) {
+//     console.log("Pagaliau nusipirkau nauja pc", JSON.stringify(value));
+// }); pazadas.then(function (reason) {
+//     console.log("Negaliu nusipirkti, nes ", reason);
+// }); pazadas.finally(function () {
+//     console.log(
+//         "Vis tiek lalalala"
+//     );
+// });
+
+let iSusitikimaAtvyko;
+iSusitikimaAtvyko = false;
+promise3 = new Promise(function (resolve, reject) {
+    if (iSusitikimaAtvyko) {
+        resolve("Jis laikosi savo zodzio");
     } else {
-        reject("Neturi pakankamai pinigu");
+        reject("Sis zmogus nesilaiko savo zodzio");
     }
-}); pazadas.then(function (value) {
-    console.log("Pagaliau nusipirkau nauja pc", JSON.stringify(value));
-}); pazadas.then(function (reason) {
-    console.log("Negaliu nusipirkti, nes ", reason);
-}); pazadas.finally(function () {
-    console.log(
-        "Vis tiek lalalala"
-    );
 });
+console.log(promise3);
